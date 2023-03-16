b1a8c3790cd9c8abaf92ae6ed66cd6001be26e9c
Leo Elmecker <lelmeckerpla@bloomberg.net>
2023-03-16 16:55:22 +0000

    Add some test changes
    Signed-off-by: Leo Elmecker <lelmeckerpla@bloomberg.net>

--- tests/cases/compiler/dissallowSymbolAsWeakType.ts
+++ tests/cases/compiler/dissallowSymbolAsWeakType.ts
@@ -5,3 +5,11 @@ const s: symbol = Symbol('s');
 
 const ws = new WeakSet([s]);
 ws.add(s);
+ws.has(s);
+ws.delete(s);
+
+const wm = new WeakMap([[s, false]]);
+wm.set(s, true);
+wm.has(s);
+wm.get(s);
+wm.delete(s);
