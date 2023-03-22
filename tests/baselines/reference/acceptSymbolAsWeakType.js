0e809bfef7a0c61fcd0ec0dbe414864820bf999f
Leo Elmecker <lelmeckerpla@bloomberg.net>
2023-03-22 23:49:37 +0000

    Add testing for WeakRef and FinalizationRegistry

--- tests/baselines/reference/acceptSymbolAsWeakType.js
+++ tests/baselines/reference/acceptSymbolAsWeakType.js
@@ -3,8 +3,35 @@ const s: symbol = Symbol('s');
 
 const ws = new WeakSet([s]);
 ws.add(s);
+ws.has(s);
+ws.delete(s);
+
+const wm = new WeakMap([[s, false]]);
+wm.set(s, true);
+wm.has(s);
+wm.get(s);
+wm.delete(s);
+
+const wr = new WeakRef(s);
+wr.deref();
+
+const f = new FinalizationRegistry<symbol>(() => {});
+f.registerSymbol(s, null);
+f.unregisterSymbol(s);
 
 //// [acceptSymbolAsWeakType.js]
 const s = Symbol('s');
 const ws = new WeakSet([s]);
 ws.add(s);
+ws.has(s);
+ws.delete(s);
+const wm = new WeakMap([[s, false]]);
+wm.set(s, true);
+wm.has(s);
+wm.get(s);
+wm.delete(s);
+const wr = new WeakRef(s);
+wr.deref();
+const f = new FinalizationRegistry(() => { });
+f.registerSymbol(s, null);
+f.unregisterSymbol(s);
--- tests/cases/compiler/dissallowSymbolAsWeakType.ts
+++ tests/cases/compiler/dissallowSymbolAsWeakType.ts
@@ -13,3 +13,10 @@ wm.set(s, true);
 wm.has(s);
 wm.get(s);
 wm.delete(s);
+
+const wr = new WeakRef(s);
+wr.deref();
+
+const f = new FinalizationRegistry(() => {});
+f.register(s, null);
+f.unregister(s);
